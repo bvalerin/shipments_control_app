7bbb24a0b365e7744b39eef367d4ed623702e101
Bryan Valerin <bryanvalerin@gmail.com>
2021-03-06 13:41:45 -0600

    Begin PDF design

--- resources/views/shipment/pdf/shipment_pdf.blade.php
+++ resources/views/shipment/pdf/shipment_pdf.blade.php
@@ -1,7 +1,34 @@
-@php
-    var_dump($shipment);
-    echo "<hr>";
-    var_dump($customer);
-    echo "<hr>";
-    var_dump($driver);
-@endphp
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+
+        html{
+            margin: 0;
+            padding: 0;
+        }
+
+        .bordered {
+            border: 1px solid;
+        }
+
+    </style>
+</head>
+<body>
+    
+    <table class="bordered">
+
+        <tr >
+            <td class="bordered">
+                Cliente: 
+            </td>
+
+            <td>
+                {{ $customer->name }}
+            </td>
+
+        </tr>
+    </table>
+
+</body>
+</html>
